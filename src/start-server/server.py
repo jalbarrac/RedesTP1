45d9898d328ccde038cbecf978124f6711d9112c
Dario Albarracin <dalbarracin@fi.uba.ar>
2026-09-20 23:11:58 -0300

    upload con S&W v1

--- src/start-server/server.py
+++ src/start-server/server.py
@@ -1,11 +1,55 @@
 from socket import *
+from enum import IntEnum
+
+class MessageCodes(IntEnum):
+	REQUEST_UPLOAD = 1
+	ACCEPT_UPLOAD = 2
+	REJECT_UPLOAD = 3
+	DATA_SEND = 4
+	DATA_ACK = 5
+	REQUEST_DOWNLOAD = 6
+	REJECT_DOWNLOAD = 7
+	DATA_DONE = 8
+
+def filename_ok(string):
+	return True
+
+def filesize_ok(size):
+	return size < 1073741824
+
 server_port = 54321
 server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind(('', server_port))
 print("Listening...")
+
 while True:
-	message, client_address = server_socket.recvfrom(2048)
-	modifiedMessage = message.decode().upper()
-	server_socket.sendto(modifiedMessage.encode(), client_address)
+	message, client_address = server_socket.recvfrom(1024)
+
+	match message[0]:
+		case MessageCodes.REQUEST_UPLOAD:
+			if not filename_ok(message[6:].decode()):
+				response = bytes([MessageCodes.REJECT_UPLOAD]) + "Error - Filename not ok.".encode()
+			elif not filesize_ok(int.from_bytes(message[2:6], 'big')):
+				response = bytes([MessageCodes.REJECT_UPLOAD]) + "Error - File too big.".encode()
+			else:
+				response = bytes([MessageCodes.ACCEPT_UPLOAD])
+		case _:
+			response = bytes([MessageCodes.REJECT_UPLOAD]) + "Error - Bad request.".encode()
+
+	server_socket.sendto(response, client_address)
+
+	#TO DO: variar segun el protocolo
+	f = open(message[6:].decode(), 'ba')
+
+	while True:
+		message, client_address = server_socket.recvfrom(1024)
+		if message[0] == MessageCodes.DATA_SEND:
+			f.write(message[5:])
+			response = bytes([MessageCodes.DATA_ACK]) + message[1:5]
+			server_socket.sendto(response, client_address)
+		elif message[0] == MessageCodes.DATA_DONE:
+			break
+
+	f.close()
 
 
--- src/upload/upload.py
+++ src/upload/upload.py
@@ -1,5 +1,17 @@
 import argparse
+from enum import IntEnum
 from socket import *
+import os
+
+class MessageCodes(IntEnum):
+	REQUEST_UPLOAD = 1
+	ACCEPT_UPLOAD = 2
+	REJECT_UPLOAD = 3
+	DATA_SEND = 4
+	DATA_ACK = 5
+	REQUEST_DOWNLOAD = 6
+	REJECT_DOWNLOAD = 7
+	DATA_DONE = 8
 
 parser = argparse.ArgumentParser(prog="upload", description="Upload a file.")
 group = parser.add_mutually_exclusive_group()
@@ -13,12 +25,44 @@ parser.add_argument("-r", "--protocol", help="error recovery protocol")
 
 args = parser.parse_args()
 
+#TO DO: Validar que el archivo exista.
 
-server_name = args.host
-server_port = args.port
+if not args.src:
+    if "./" not in args.name:
+        args.src = "./"
+    else:
+        args.src = ""
+
 client_socket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence: ')
-client_socket.sendto(message.encode(), (server_name, server_port))
-modifiedMessage, server_address = client_socket.recvfrom(2048)
-print(modifiedMessage.decode())
+
+file_size_bytes = os.path.getsize(args.src+args.name).to_bytes(4,'big')
+
+#solicitar inicio de upload
+message = bytes([MessageCodes.REQUEST_UPLOAD,1]) + file_size_bytes + args.name.encode()
+client_socket.sendto(message, (args.host, args.port))
+
+response, server_address = client_socket.recvfrom(1024)
+
+if response[0] == MessageCodes.REJECT_UPLOAD:
+    print(response[1:].decode())
+
+#TO DO: Generalizar a ambos protocolos
+elif response[0] == MessageCodes.ACCEPT_UPLOAD:
+    f = open(args.src+args.name, 'br')
+    data = f.read(1019)
+    seqnum = 0
+    while data:
+        message = bytes([MessageCodes.DATA_SEND]) + seqnum.to_bytes(4,'big') + data
+        client_socket.sendto(message, (args.host, args.port))
+        response, server_address = client_socket.recvfrom(1024)
+        
+        #si recibo ACK, avanzo
+        #TO DO: Timeout
+        if response[0] == MessageCodes.DATA_ACK and int.from_bytes(response[1:], 'big') == seqnum:
+            data = f.read(1019)
+            seqnum = seqnum + 1
+
+    message = bytes([MessageCodes.DATA_DONE])
+    client_socket.sendto(message, (args.host, args.port))
+    f.close()
 client_socket.close()
